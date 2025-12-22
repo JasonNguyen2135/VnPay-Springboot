bf11cb76f4635e1bf45bea8595f47b44e7a7c017
nguyentrinh2135 <caocaonguyentrinh210305@gmail.com>
2025-12-22 17:28:07 +0700

    add healthcheck

---
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
